9ef16f70681d509da5ddebd16298280219c64176
Carlos <carloslira.adm@gmail.com>
2026-06-17 21:43:45 -0300

    Criando crachá virtual

---
+++ Atividade5/formulario.js
@@ -0,0 +1,8 @@
+ function imprimirnome() {
+                window.alert("Usuário cadastrado")
+                let nome = document.getElementById("nome").value;
+
+                console.log("Seu nome é: ", nome)
+
+                alert("Aluno cadastrado!\n\nNome do aluno: " + nome)
+            }
---
+++ Atividade6/cracha.js
@@ -0,0 +1,16 @@
+
+let funcionario = {
+    Nome: document.getElementById("funcionario.Nome"),
+    Idade: document.getElementById("funcionario.Idade"),
+    Cargo: document.getElementById("funcionario.Cargo"),
+    Departamento: document.getElementById("funcionario.Departamento"),
+    Acesso_Restrito: document.getElementById("funcionario.Acesso_Restrito")
+};
+console.log("Nome: ", funcionario.Nome);
+console.log("Idade: ", funcionario.Idade);
+console.log("Cargo: ", funcionario.Cargo);
+console.log("Departamento: ", funcionario.Departamento);
+console.log("Tem acesso restrito? : ", funcionario.Acesso_Restrito)
+
+alert("Cadastro realizado com sucesso!")
+
