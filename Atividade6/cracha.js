784600732f34a5d2da60f6f996608096248d087a
Carlos <carloslira.adm@gmail.com>
2026-06-18 20:11:10 -0300

    Melhorando crachá virtual

--- Atividade6/cracha.js
+++ Atividade6/cracha.js
@@ -1,16 +1,25 @@
+function imprimirnome() {
+    let Nome = document.getElementById("Nome").value;
+    let Idade = document.getElementById("Idade").value;
+    let Cargo = document.getElementById("Cargo").value;
+    let Departamento = document.getElementById("Departamento").value;
+    let Acesso_Restrito = document.getElementById("Acesso_Restrito").value;
 
-let funcionario = {
-    Nome: document.getElementById("funcionario.Nome"),
-    Idade: document.getElementById("funcionario.Idade"),
-    Cargo: document.getElementById("funcionario.Cargo"),
-    Departamento: document.getElementById("funcionario.Departamento"),
-    Acesso_Restrito: document.getElementById("funcionario.Acesso_Restrito")
-};
-console.log("Nome: ", funcionario.Nome);
-console.log("Idade: ", funcionario.Idade);
-console.log("Cargo: ", funcionario.Cargo);
-console.log("Departamento: ", funcionario.Departamento);
-console.log("Tem acesso restrito? : ", funcionario.Acesso_Restrito)
+console.log("Nome(String): ", Nome);
+console.log("Idade(Number): ", Idade);
+console.log("Cargo(String): ", Cargo);
+console.log("Departamento(String): ", Departamento);
+console.log("Acesso_Restrito(Boolean): ", Acesso_Restrito);
 
-alert("Cadastro realizado com sucesso!")
+
+window.alert(
+    "Cadastro finalizado com sucesso! \n\n" +
+    "Dados do Crachá: \n\n" +
+    "Nome: " + Nome + "\n" +
+    "Idade: " + Idade + "\n" +
+    "Cargo: " + Cargo + "\n" +
+    "Departamento: " + Departamento + "\n" +
+    "Acesso Restrito: " + Acesso_Restrito + "\n"
+)
+}
 
