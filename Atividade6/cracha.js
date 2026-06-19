d9d6d2260cc1eafc2118066cf484a3c2bc19cb4b
Carlos <carloslira.adm@gmail.com>
2026-06-18 21:41:38 -0300

    Mais algumas melhorias

--- Atividade6/cracha.js
+++ Atividade6/cracha.js
@@ -5,21 +5,26 @@ function imprimirnome() {
     let Departamento = document.getElementById("Departamento").value;
     let Acesso_Restrito = document.getElementById("Acesso_Restrito").value;
 
-console.log("Nome(String): ", Nome);
-console.log("Idade(Number): ", Idade);
-console.log("Cargo(String): ", Cargo);
-console.log("Departamento(String): ", Departamento);
-console.log("Acesso_Restrito(Boolean): ", Acesso_Restrito);
+    console.log("Nome(String): ", Nome);
+    console.log("Idade(Number): ", Idade);
+    console.log("Cargo(String): ", Cargo);
+    console.log("Departamento(String): ", Departamento);
+    console.log("Acesso_Restrito(Boolean): ", Acesso_Restrito);
 
 
-window.alert(
-    "Cadastro finalizado com sucesso! \n\n" +
-    "Dados do Crachá: \n\n" +
-    "Nome: " + Nome + "\n" +
-    "Idade: " + Idade + "\n" +
-    "Cargo: " + Cargo + "\n" +
-    "Departamento: " + Departamento + "\n" +
-    "Acesso Restrito: " + Acesso_Restrito + "\n"
-)
+    window.alert(
+        "Cadastro finalizado com sucesso! \n\n" +
+        "Dados do Crachá: \n\n" +
+        "Nome: " + Nome + "\n" +
+        "Idade: " + Idade + "\n" +
+        "Cargo: " + Cargo + "\n" +
+        "Departamento: " + Departamento + "\n" +
+        "Acesso Restrito: " + Acesso_Restrito + "\n"
+    );
+    if (Acesso_Restrito) {/// se verdadeiro
+        window.location.href = "pages/gerente.html"
+    } else {/// se falso
+        window.location.href = "pages/funcionario.html"
+    }
 }
 
