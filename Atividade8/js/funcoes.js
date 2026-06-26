14a05a544ef7c03a721e9cef4ec95d0ab48dec93
Carlos <carloslira.adm@gmail.com>
2026-06-25 21:32:58 -0300

    Calculando imposto de importação

--- Atividade8/js/funcoes.js
+++ Atividade8/js/funcoes.js
@@ -42,9 +42,65 @@ function divisao() {
     salvarHistorico("Divisão", primeiroNumero, segundoNumero, resultado);
 }
 
+function elevado() {
+    let primeiroNumero = document.getElementById("primeiro-numero").value;
+    let segundoNumero = document.getElementById("segundo-numero").value;
+
+    let resultado = Number(primeiroNumero) ** Number(segundoNumero);
+
+    document.getElementById("resultado").innerText = resultado;
+
+    salvarHistorico("Elevado", primeiroNumero, segundoNumero, resultado);
+}
+
 function salvarHistorico(nomeDaFuncao, num1, num2, resultado) {
     console.log("Função: " + nomeDaFuncao);
     console.log("Primeiro numero: " + num1 + ", SegundoNumero: " + num2);
+    console.log("Resultado: " + resultado);
+    console.log("--------------------------------------");
+
+    let operacao = {
+        funcao: nomeDaFuncao,
+        numero1: num1,
+        numero2: num2,
+        resultado: resultado
+    };
+
+    let historicoLocal = JSON.parse(localStorage.getItem("historicoCalculos")) || [];
+    historicoLocal.push(operacao);
+
+    localStorage.setItem("historicoCalculos", JSON.stringify(historicoLocal));
+}
+
+/* Convertendo em dólar*/
+
+function conversao() {
+    let valorreal = document.getElementById("valor-real").value;
+    let valordolar = document.getElementById("valor-dolar").value;
+
+    let resultado = Number(valorreal) / Number(valordolar);
+
+    document.getElementById("resultado-conversao").innerText = resultado;
+
+    salvarConversao("Conversao", valorreal, valordolar, resultado);
+}
+
+function calcularImposto(valor){
+    let valorFinal;
+
+    if (valor <= 50){
+        valorFinal = valor * 1.20;
+    } else{
+        let comPrimeiroImposto = valor * 1.20;
+        valorFinal = comPrimeiroImposto * 1.90;
+    }
+    return valorfinal
+
+}
+
+function salvarConversao(nomeDaFuncao, num1, num2, resultado) {
+    console.log("Função: " + nomeDaFuncao);
+    console.log("Primeiro Real: " + num1 + ", Valor Dolar: " + num2);
     console.log("Resultado: " + resultado);
     console.log("--------------------------------------");
 
